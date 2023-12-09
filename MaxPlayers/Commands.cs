cd5e6cf6a036a055a1f0a018105dd268c8518717
Mest <mestevenemail@gmail.com>
2023-12-09 04:57:45 +0000

    Add project files.

---
+++ MaxPlayers/Commands.cs
@@ -0,0 +1,41 @@
+﻿using CommandHandler.Chat.Router;
+using CommandHandler.Utilities;
+using Photon.Pun;
+
+namespace MaxPlayers
+{
+    internal class CountCommand : ChatCommand
+    {
+        public override string[] CommandAliases()
+            => new string[] { "playercount", "pc" };
+
+        public override string Description()
+            => "Count of players.";
+
+        public override void Execute(string arguments)
+        {
+            if (!PhotonNetwork.InRoom) return;
+            Messaging.Echo($"Players: {PhotonNetwork.CurrentRoom.PlayerCount} / {PhotonNetwork.CurrentRoom.MaxPlayers}");
+        }
+    }
+    internal class IncreaseCommand : ChatCommand
+    {
+        public override string[] CommandAliases()
+            => new string[] { "setplayercount", "spc" };
+
+        public override string Description()
+            => "Count of players.";
+
+        public override void Execute(string arguments)
+        {
+            string[] args = arguments.Split(' ');
+            if (args.Length < 1 || !PhotonNetwork.IsMasterClient) return;
+            if (int.TryParse(args[0], out int value))
+            {
+                Plugin.PlayerCount = (byte)value;
+            }
+            if (PhotonNetwork.InRoom) PhotonNetwork.CurrentRoom.MaxPlayers = Plugin.PlayerCount;
+            Messaging.Echo($"Max player count: desired-{Plugin.PlayerCount} : current-{PhotonNetwork.CurrentRoom.MaxPlayers}");
+        }
+    }
+}
---
+++ MaxPlayers/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace MaxPlayers
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static readonly Harmony Harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        public static byte PlayerCount = 4;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.PatchAll();
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is patched!");
+        }
+    }
+}
