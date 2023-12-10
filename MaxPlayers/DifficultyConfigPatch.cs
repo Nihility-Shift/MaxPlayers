585b17e1e98e5da65ef3f460675f14cf99421eec
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2023-12-09 20:04:24 -0800

    Patched config getters for playercount difficulty.

---
+++ MaxPlayers/DifficultyConfigPatch.cs
@@ -0,0 +1,31 @@
+﻿using Gameplay.MissionDifficulty;
+using HarmonyLib;
+
+namespace MaxPlayers
+{
+    [HarmonyPatch(typeof(QuestDifficultyContainer), "GetConfig")]
+    internal class DifficultyConfigPatch
+    {
+        [HarmonyPrefix]
+        static void GetConfigPrefix(ref Difficulty df)
+        {
+            if((int)df >= 4)
+            {
+                df = (Difficulty)4;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DifficultyPlayerCountTable), "GetConfig")]
+    internal class DifficultyPlayerCountTablePatch
+    {
+        [HarmonyPrefix]
+        static void GetConfigPrefix(ref int playerCount)
+        {
+            if (playerCount >= 4)
+            {
+                playerCount = 4;
+            }
+        }
+    }
+}
