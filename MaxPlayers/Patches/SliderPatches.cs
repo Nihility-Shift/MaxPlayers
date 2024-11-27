43a6225419e6a3536087988ea8c4f2c61c18b195
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-11-27 01:53:34 -0800

    Added Slider Patches

---
+++ MaxPlayers/Patches/SliderPatches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+
+namespace MaxPlayers.Patches
+{
+    [HarmonyPatch(typeof(MatchMakingMenu), MethodType.Constructor)]
+    internal class MatchmakingPanelSlider
+    {
+        static void Postfix(MatchMakingMenu __instance)
+        {
+            __instance._playerLimit.defaultValue = Limits.DefaultPlayerLimit;
+            __instance._playerLimit.SetValue(Limits.DefaultPlayerLimit);
+            __instance._playerLimit.max = Limits.SliderLimit;
+        }
+    }
+
+    [HarmonyPatch(typeof(LobbyPanel), MethodType.Constructor)]
+    internal class LobbyPanelSlider
+    {
+        static void Postfix(LobbyPanel __instance)
+        {
+            __instance._playerLimit.defaultValue = Limits.DefaultPlayerLimit;
+            __instance._playerLimit.SetValue(Limits.DefaultPlayerLimit);
+            __instance._playerLimit.max = Limits.SliderLimit;
+        }
+    }
+}
