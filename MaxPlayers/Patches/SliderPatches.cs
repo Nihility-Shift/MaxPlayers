67e33222787c77e538f777b05dd80656b2c4ee40
TheRealMesteven <mestevenyoutubing@gmail.com>
2024-11-30 05:23:51 +0000

    Fixed the sliders

--- MaxPlayers/Patches/SliderPatches.cs
+++ MaxPlayers/Patches/SliderPatches.cs
@@ -1,26 +1,69 @@
 ﻿using HarmonyLib;
+using HarmonyLib.Tools;
+using System;
+using System.Diagnostics;
+using System.Text;
+using UI;
+using UI.Settings;
+using UnityEngine.UIElements;
 
 namespace MaxPlayers.Patches
 {
-    [HarmonyPatch(typeof(MatchMakingMenu), MethodType.Constructor)]
-    internal class MatchmakingPanelSlider
+    [HarmonyPatch(typeof(IntSetting), MethodType.Constructor, new Type[] { typeof(int), typeof(int), typeof(int) })]
+    internal class IntSettingOverride
     {
-        static void Postfix(MatchMakingMenu __instance)
+        static void Prefix(IntSetting __instance, ref int defaultValue, ref int min, ref int max)
         {
-            __instance._playerLimit.defaultValue = Limits.DefaultPlayerLimit;
-            __instance._playerLimit.SetValue(Limits.DefaultPlayerLimit);
-            __instance._playerLimit.max = Limits.SliderLimit;
+            var methodInfo = new StackTrace().GetFrame(2).GetMethod();
+            if (methodInfo.ReflectedType.Name == "LobbyPanel" || methodInfo.ReflectedType.Name == "MatchMakingMenu" || methodInfo.ReflectedType.Name == "MatchmakingRoomSetup")
+            {
+                defaultValue = Limits.DefaultPlayerLimit;
+                max = Limits.SliderLimit;
+            }
+            //else if (max == 4) { BepinPlugin.Log.LogInfo($"PATCH THIS {methodInfo.ReflectedType.Name} IntSetting Values: {defaultValue}, {min}, {max}"); }
         }
-    }
-
-    [HarmonyPatch(typeof(LobbyPanel), MethodType.Constructor)]
-    internal class LobbyPanelSlider
-    {
-        static void Postfix(LobbyPanel __instance)
+        /*static void Postfix(IntSetting __instance, ref int defaultValue, ref int min, ref int max)
         {
-            __instance._playerLimit.defaultValue = Limits.DefaultPlayerLimit;
-            __instance._playerLimit.SetValue(Limits.DefaultPlayerLimit);
-            __instance._playerLimit.max = Limits.SliderLimit;
+            var methodInfo = new StackTrace().GetFrame(2).GetMethod();
+            if (methodInfo.ReflectedType.Name == "LobbyPanel" || methodInfo.ReflectedType.Name == "MatchMakingMenu" || methodInfo.ReflectedType.Name == "MatchmakingRoomSetup")
+            {
+                BepinPlugin.Log.LogInfo($"{methodInfo.ReflectedType.Name} IntSetting Values: {defaultValue}, {min}, {max}");
+            }
+        }*/
+    }
+    [HarmonyPatch(typeof(SliderIntSettingEntryVE), "Init")]
+    internal class SliderIntSettingEntryVEOverride
+    {
+        static void Postfix(SliderIntSettingEntryVE __instance)
+        {
+            //BepinPlugin.Log.LogInfo($"Slider - {__instance.DisplayName}, {__instance.settingsLabel}, {__instance.SettingName} | {__instance.DisplayName.Contains("player_limit")} && {__instance.setting != null} - {__instance.setting.max}");
+            if (__instance.DisplayName.Contains("player_limit") && __instance.setting != null)
+            {
+                __instance.HighValue = __instance.setting.max;
+                __instance.slider.highValue = __instance.setting.max;
+
+                VisualElement stepContainer = __instance.slider.parent.Q(name: "StepContainer", className: null);
+                if (stepContainer != null)
+                {
+                    stepContainer.Clear();
+
+                    int num = __instance.setting.max - __instance.LowValue + 2;
+                    for (int i = __instance.LowValue; i < num; i++)
+                    {
+                        VisualElement stepElement = new VisualElement
+                        {
+                            name = i.ToString()
+                        };
+                        int stepValue = i;
+                        stepElement.RegisterClickEvent(delegate
+                        {
+                            __instance.setting.SetValue(stepValue, false);
+                            __instance.Reload();
+                        });
+                        stepContainer.Add(stepElement);
+                    }
+                }
+            }
         }
     }
 }
