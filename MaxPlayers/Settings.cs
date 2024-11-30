030d8ef6ddd065b623ae324bc1e2aab364142d7b
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-11-30 12:18:06 -0800

    Set DefaultPlayerLimit to 20

--- MaxPlayers/Settings.cs
+++ MaxPlayers/Settings.cs
@@ -5,7 +5,7 @@ namespace MaxPlayers
     internal class Settings
     {
         internal static ConfigEntry<byte> DefaultPlayerLimit;
-        internal const byte defaultplayerlimit = 16;
+        internal const byte defaultplayerlimit = 20;
         internal static ConfigEntry<byte> SliderLimit;
         internal const byte defaultsliderlimit = 20;
         internal static ConfigEntry<bool> ChairStartEnabled;
