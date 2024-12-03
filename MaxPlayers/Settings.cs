9902ad71fa044aed5f0fbfe6d129c6d092e60c62
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-02 20:15:22 -0800

    Updated default settings.

--- MaxPlayers/Settings.cs
+++ MaxPlayers/Settings.cs
@@ -5,9 +5,9 @@ namespace MaxPlayers
     internal class Settings
     {
         internal static ConfigEntry<byte> DefaultPlayerLimit;
-        internal const byte defaultplayerlimit = 20;
+        internal const byte defaultplayerlimit = 8;
         internal static ConfigEntry<byte> SliderLimit;
-        internal const byte defaultsliderlimit = 20;
+        internal const byte defaultsliderlimit = 24;
         internal static ConfigEntry<bool> ChairStartEnabled;
     }
 }
