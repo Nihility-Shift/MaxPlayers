b7329e158c9edb4a6c93fabfadba77cbcb31efd3
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-08 13:22:11 -0800

    Updated Documentation

--- MaxPlayers/Patches/SliderPatches.cs
+++ MaxPlayers/Patches/SliderPatches.cs
@@ -1,25 +1,10 @@
 ﻿using HarmonyLib;
-using System;
-using System.Diagnostics;
 using UI;
 using UI.Settings;
 using UnityEngine.UIElements;
 
 namespace MaxPlayers.Patches
 {
-    /*[HarmonyPatch(typeof(IntSetting), MethodType.Constructor, new Type[] { typeof(int), typeof(int), typeof(int) })]
-    internal class IntSettingOverride
-    {
-        static void Prefix(IntSetting __instance, ref int defaultValue, ref int min, ref int max)
-        {
-            var methodName = new StackTrace().GetFrame(2).GetMethod().ReflectedType.Name;
-            if (methodName == "LobbyPanel" || methodName == "MatchMakingMenu" || methodName == "MatchmakingRoomSetup")
-            {
-                defaultValue = Limits.DefaultPlayerLimit;
-                max = Limits.SliderLimit;
-            }
-        }
-    }*/
     [HarmonyPatch(typeof(SliderIntSettingEntryVE), "Init")]
     internal class SliderIntSettingEntryVEOverride
     {
@@ -41,62 +26,4 @@ namespace MaxPlayers.Patches
             }
         }
     }
-
-    /*
-    [HarmonyPatch(typeof(MatchMakingMenu), "Init")]
-    internal class MatchmakingPanelSlider
-    {
-        static void Prefix(MatchMakingMenu __instance)
-        {
-            __instance._playerLimit.max = Limits.SliderLimit;
-            __instance._playerLimit.defaultValue = Limits.DefaultPlayerLimit;
-            __instance._playerLimit.SetValue(Limits.DefaultPlayerLimit);
-        }
-
-        static void Postfix(MatchMakingMenu __instance)
-        {
-            var playerLimitVE = __instance._playerLimitVE;
-            playerLimitVE.HighValue = Limits.SliderLimit;
-            playerLimitVE.Init(__instance._playerLimit);
-            VisualElement stepContainer = playerLimitVE.Q("StepContainer", null, null);
-            stepContainer.RemoveChildren();
-            playerLimitVE.SetupContainers();
-        }
-    }
-    
-    [HarmonyPatch(typeof(LobbyPanel), MethodType.Constructor, new Type[] { typeof(VisualElement), typeof(VisualTreeAsset) })]
-    internal class LobbyPanelSlider
-    {
-        static void Postfix(LobbyPanel __instance)
-        {
-            SliderIntSettingEntryVE playerLimitVE = __instance._playerLimitVE;
-
-            __instance._playerLimit.max = Limits.SliderLimit;
-            __instance._playerLimit.defaultValue = Limits.DefaultPlayerLimit;
-            __instance._playerLimit.SetValue(Limits.DefaultPlayerLimit);
-            playerLimitVE.HighValue = Limits.SliderLimit;
-            playerLimitVE.Init(__instance._playerLimit);
-            VisualElement stepContainer = playerLimitVE.Q("StepContainer", null, null);
-            stepContainer.RemoveChildren();
-            playerLimitVE.SetupContainers();
-            BepinPlugin.Log.LogInfo($"LPVE set to {playerLimitVE.HighValue}");
-        }
-    }aw
-    
-    [HarmonyPatch(typeof(MatchmakingRoomSetup), MethodType.Constructor, new Type[] { typeof(VisualElement) })]
-    internal class MatchMakingRoomSetupSlider
-    {
-        static void Postfix(MatchmakingRoomSetup __instance)
-        {
-            SliderIntSettingEntryVE playerLimitVE = __instance.playerLimitVE;
-
-            __instance.playerLimit.max = Limits.SliderLimit;
-            __instance.playerLimit.defaultValue = Limits.DefaultPlayerLimit;
-            __instance.playerLimit.SetValue(Limits.DefaultPlayerLimit);
-            playerLimitVE.HighValue = Limits.SliderLimit;
-            playerLimitVE.Init(__instance.playerLimit);
-            playerLimitVE.SetupContainers();
-            BepinPlugin.Log.LogInfo($"MMRSVE set to {playerLimitVE.HighValue}");
-        }
-    }*/
 }
--- MaxPlayers/ReleaseFiles/CHANGELOG.md
+++ MaxPlayers/ReleaseFiles/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.1.2
+- Fixed issue with hull damage at player counts of 4
+
 ## 1.1.1
 - Disabled vanilla error logs due to too many players
 
