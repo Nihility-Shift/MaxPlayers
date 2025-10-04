19a5fc1cb0cc608d9a7792d60ddb6caa5bd8a60a
Nagord <46509577+Nagord@users.noreply.github.com>
2025-10-03 20:12:25 -0700

    Added old code for attempted vanilla fix.

---
+++ MaxPlayers/Patches/BullshitVanillaRejoinFuckupPatch.cs
@@ -0,0 +1,28 @@
+﻿using CG.GameLoopStateMachine.GameStates;
+using HarmonyLib;
+using PlatformAbstractionLayer;
+using System;
+
+namespace MaxPlayers.Patches
+{
+    //PAL attempts to join the lobby after the user has already joined the lobby. This sets the PAL currentLobbyID to the current lobby for joining,
+    // which should prevent a duplicate join. PlatformService.ExecuteJoinLobby checks that the currentLobbyID does equal the current room.
+
+    // Didn't work -_-.
+
+    [HarmonyPatch(typeof(GSJoinExternalGame), "SetPhotonRoomToJoin")]
+    internal class BullshitVanillaRejoinFuckupPatch
+    {
+        static void Postfix(string room)
+        {
+            try
+            {
+                PlatformService.Instance.currentLobbyID = new LobbyID(room);
+            }
+            catch(Exception ex)
+            {
+                BepinPlugin.Log.LogError(ex);
+            }
+        }
+    }
+}
